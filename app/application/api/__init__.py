a2c023cfddfe9b9cd00ddb2f55ae9748af408fb5
nim4n136 <nim4n136@gmail.com>
2021-09-24 02:00:22 +0700

    add handler not found

--- app/application/api/__init__.py
+++ app/application/api/__init__.py
@@ -1,3 +1,10 @@
 from .v1 import api_v1
+from app.application.http.response import Response
+
 def register_api(app):
     app.register_blueprint(api_v1, url_prefix="/api/v1")
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return Response().notfound()
+
